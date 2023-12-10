9fd5731152e1f8f6b1fd631328a7592c9fb80eed
babucarr32 <baboucarry@gmai.com>
2023-12-10 12:34:23 +0000

    Update code

--- src/pyclitable/pyCliTable.py
+++ src/pyclitable/pyCliTable.py
@@ -1,40 +1,43 @@
-# from style import style
 from style import Color
 
 color = Color()
 
 
-def markup(arg, markup):
-    if markup.lower() == "blue":
+def markup(arg, markup="", table_color=""):
+    if markup.lower() == "blue" or table_color.lower() == "blue":
         return color.Blue(arg)
-    if markup.lower() == "red":
+    if markup.lower() == "red" or table_color.lower() == "red":
         return color.Red(arg)
-    if markup.lower() == "green":
+    if markup.lower() == "green" or table_color.lower() == "green":
         return color.Green(arg)
-    if markup.lower() == "yellow":
+    if markup.lower() == "yellow" or table_color.lower() == "yellow":
         return color.Yellow(arg)
-    if markup.lower() == "magenta":
+    if markup.lower() == "magenta" or table_color.lower() == "magenta":
         return color.Magenta(arg)
-    if markup.lower() == "cyan":
+    if markup.lower() == "cyan" or table_color.lower() == "cyan":
         return color.Cyan(arg)
-    if markup.lower() == "white":
+    if markup.lower() == "white" or table_color.lower() == "white":
         return color.White(arg)
-    if markup.lower() == "underline":
+    if markup.lower() == "gray" or table_color.lower() == "gray":
+        return color.Gray(arg)
+    if markup.lower() == "underline" or table_color.lower() == "underline":
         return color.Underline(arg)
-    if markup.lower() == "bold":
+    if markup.lower() == "bold" or table_color.lower() == "bold":
         return color.Bold(arg)
-    if markup.lower() == "italic":
+    if markup.lower() == "italic" or table_color.lower() == "italic":
         return color.Italic(arg)
 
 
-def table(data, config='', header_config='', word_space=2):
+def table(data, config='', header_config='', table_color="gray", word_space=2):
     """ data = [
             {"Name": ["Foo", "Fooo", "Fooooo"]},
             {"Surname": ["Bar", "Baar", "Baaar"]},
             {"Gender": ["Male", "Female", "Male"]
         }
 
-    config = ["red", "green", "yellow", "blue", "magenta", "cyan", "white", "underline", "bold", "italy"]
+    config = ["red", "green", "yellow", "blue", "magenta", "cyan", "gray", "white", "underline", "bold", "italy"]
+    header_config = ["red", "green", "yellow", "blue", "magenta", "cyan", "gray", "white", "underline", "bold", "italy"]
+    table_color = "red" | "green" | "yellow" | "blue" | "magenta" | "cyan" | "gray" | "white" | "underline" | "bold" | "italy"
     """
     chars = {
         'top': '─',
@@ -95,7 +98,7 @@ def table(data, config='', header_config='', word_space=2):
                       (max(max_len_list[loop_count][i]) - len(i)))
         table_header += (" " * word_space) + \
             temp_table_row + (" " * (word_space +
-                                     (max(max_len_list[loop_count][i]) - len(i)))) + chars["right"]
+                                     (max(max_len_list[loop_count][i]) - len(i)))) + markup(chars["right"], table_color=table_color)
         temp_table_row = ""
 
         header_top_line += chars["top"] * (len((" " * word_space) +
@@ -105,19 +108,23 @@ def table(data, config='', header_config='', word_space=2):
         header_bottom_line += chars["bottom"] * (len((" " * word_space) +
                                                      i + (" " * (word_space +
                                                           (max(max_len_list[loop_count][i]) - len(i)))) + chars["right"]) - 1)
-        table_bottom_line += chars["bottom"] * (len((" " * word_space) +
-                                                    i + (" " * (word_space +
-                                                                (max(max_len_list[loop_count][i]) - len(i)))) + chars["bottom-right"]) - 1)
+        lines = chars["bottom"] * (len((" " * word_space) +
+                                       i + (" " * (word_space +
+                                                   (max(max_len_list[loop_count][i]) - len(i)))) + chars["bottom-right"]) - 1)
+        table_bottom_line += markup(lines,
+                                    markup="", table_color=table_color)
 
         if not i == header_list[-1]:
             header_bottom_line += chars["mid-mid"]
             header_top_line += chars["top-mid"]
-            table_bottom_line += chars["bottom-mid"]
+            table_bottom_line += markup(chars["bottom-mid"],
+                                        markup="", table_color=table_color)
 
         else:
             header_bottom_line += chars["right-mid"]
             header_top_line += chars["top-right"]
-            table_bottom_line += chars["bottom-right"]
+            table_bottom_line += markup(chars["bottom-right"],
+                                        markup="", table_color=table_color)
 
         loop_count += 1
 
@@ -125,14 +132,19 @@ def table(data, config='', header_config='', word_space=2):
     for x in range(len(row_list[0])):
         rows.append({str(x): []})
         for i in row_list:
-            rows[x][str(x)].append(i[x])
+            try:
+                rows[x][str(x)].append(i[x])
+            except IndexError:
+                rows, x, rows[x][str(x)].append("*****")
+                continue
 
     for x in rows:
         loop_start = True
         for i in rows[sec_loop_count][str(sec_loop_count)]:
             temp_table_row = i
             if loop_start:
-                table_row += chars["left"]
+                table_row += markup(chars["left"],
+                                    markup="", table_color=table_color)
 
             for conf in config:
                 sec_text = markup(temp_table_row, conf)
@@ -140,7 +152,7 @@ def table(data, config='', header_config='', word_space=2):
 
             table_row += (" " * word_space) + \
                 temp_table_row + (" " * (word_space +
-                                         max(max_len_list[loop_count][header_list[loop_count]]) - len(i))) + chars["right"]
+                                         max(max_len_list[loop_count][header_list[loop_count]]) - len(i))) + markup(chars["right"], markup="", table_color=table_color)
             loop_count += 1
             loop_start = False
             temp_table_row = ""
@@ -150,6 +162,9 @@ def table(data, config='', header_config='', word_space=2):
 
         if not x == rows[-1]:
             table_row += "\n"
+            table_row += markup(
+                f'{chars["left-mid"] + header_bottom_line}',  table_color=table_color)
+            table_row += "\n"
 
-    table = f'{chars["top-left"] + header_top_line}\n{chars["left"] + table_header}\n{chars["left-mid"] + header_bottom_line}\n{table_row}\n{chars["bottom-left"] + table_bottom_line}'
+    table = f'{markup(chars["top-left"] + header_top_line, table_color=table_color)}\n{markup(chars["left"], table_color=table_color) + table_header}\n{markup(chars["left-mid"] + header_bottom_line, table_color=table_color)}\n{table_row}\n{markup(chars["bottom-left"], table_color=table_color) + table_bottom_line}'
     return table
--- src/pyclitable/style.py
+++ src/pyclitable/style.py
@@ -13,6 +13,7 @@ class Color():
         self.magenta = '\033[35m'
         self.cyan = '\033[36m'
         self.white = '\033[37m'
+        self.gray = '\033[90m'
         self.underline = '\033[4m'
         self.bold = '\033[1m'
         self.italic = '\033[3m'
@@ -46,6 +47,9 @@ class Color():
     def White(self, arg):
         return self.white + arg + '\033[0m'
 
+    def Gray(self, arg):
+        return self.gray + arg + '\033[0m'
+
     def Underline(self, arg):
         return self.underline + arg + '\033[0m'
 
@@ -57,43 +61,3 @@ class Color():
 
     def YellowBackground(self, arg):
         return self.yellowBackground + arg + '\033[0m'
-
-
-# # Text formatting
-# print("\033[1mBold Text\033[0m")  # Bold
-# print("\033[3mItalic Text\033[0m")  # Italic
-# print("\033[4mUnderline Text\033[0m")  # Underline
-# print("\033[9mStrikethrough Text\033[0m")  # Strikethrough
-
-# # Text colors
-# print("\033[31mRed Text\033[0m")  # Red text
-# print("\033[32mGreen Text\033[0m")  # Green text
-# print("\033[33mYellow Text\033[0m")  # Yellow text
-# print("\033[34mBlue Text\033[0m")  # Blue text
-# print("\033[35mMagenta Text\033[0m")  # Magenta text
-# print("\033[36mCyan Text\033[0m")  # Cyan text
-# print("\033[37mWhite Text\033[0m")  # White text
-
-# # Background colors
-# print("\033[41mRed Background\033[0m")  # Red background
-# print("\033[42mGreen Background\033[0m")  # Green background
-# print("\033[43mYellow Background\033[0m")  # Yellow background
-# print("\033[44mBlue Background\033[0m")  # Blue background
-# print("\033[45mMagenta Background\033[0m")  # Magenta background
-# print("\033[46mCyan Background\033[0m")  # Cyan background
-# print("\033[47mWhite Background\033[0m")  # White background
-
-# # Padding with spaces
-# text = "Hello"
-# padded_text = text.ljust(10)  # Left-aligned padding with spaces
-# print(f'[{padded_text}]')  # Prints "[Hello     ]"
-
-# # Padding with a specific character
-# text = "World"
-# padded_text = text.rjust(10, '_')  # Right-aligned padding with underscores
-# print(f'[{padded_text}]')  # Prints "[_____World]"
-
-# # Centered padding
-# text = "Python"
-# padded_text = text.center(15, '-')  # Center-aligned padding with dashes
-# print(f'[{padded_text}]')  # Prints "[----Python----]"
